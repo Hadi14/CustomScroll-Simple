56ae4c15083d7b601fd48027d12b9c9c6a446124
Hadi14 <asusn43@yahoo.com>
2022-10-28 14:58:55 +0330

    ok

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const d = document.querySelector('div');
+const h2 = document.querySelector('h2');
 const b = document.body;
+const ar = [
+    { Name: "Hadi", age: 10 },
+    { Name: "Reza", age: 18 },
+    { Name: "Ahmad", age: 16 },
+    { Name: "Ugf", age: 12 }
+]
+
+
 window.addEventListener('scroll', () => {
 
     console.log("***************");
@@ -13,3 +22,18 @@ window.addEventListener('scroll', () => {
     let sp = Math.round((sy / (ch - ih)) * 100)
     // console.log(sp d.style.width = sp + "%";
 })
+let x = new URLSearchParams(location.search)
+let id = x.get('id');
+console.log(id);
+
+let ag = ar.find((item) => {
+    return item.age == id
+})
+
+console.log(ag)
+if (ag) {
+    h2.innerHTML = ag.Name + "   " + ag.age;
+}
+else {
+    h2.innerHTML = "Not Found"
+}
